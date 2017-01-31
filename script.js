7697c0f2474d36cbdc14a71cffb8959fc8183ae2
Joshua <joshua@joshua-laptop.home>
2017-01-31 17:29:41 +0000

    Added Files

---
+++ script.js
@@ -0,0 +1,42 @@
+
+$('#b-con').on('click', function() {
+  $('#con').fadeToggle(200);
+});
+
+$('#b-hardware').on('click', function() {
+  $('#hardware').fadeToggle(200);
+});
+
+$('#b-topology').on('click', function() {
+  $('#topology').fadeToggle(200);
+});
+
+$('#b-protocols').on('click', function() {
+  $('#protocols').fadeToggle(200);
+});
+
+$('#b-extent').on('click', function() {
+  $('#extent').fadeToggle(200);
+});
+
+$('#b-networks').on('click', function() {
+  $('#networks').fadeToggle(200);
+});
+
+// Mouseover events
+
+$('#btn5').on('mouseover', function() {
+  $('#panel5').fadeToggle(200);
+});
+
+$('#btn6').on('mouseover', function() {
+  $('#panel6').fadeToggle(200);
+});
+
+$('#btn7').on('mouseover', function() {
+  $('#panel7').fadeToggle(200);
+});
+
+$('#btn8').on('mouseover', function() {
+  $('#panel8').fadeToggle(200);
+});
