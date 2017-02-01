334d93d65f1225be70cc25e4a413f32b4f5b16d1
jcsawyer <joshua@jcsawyer.me>
2017-02-01 18:27:06 +0000

    Improve Alot
    -Rewrote Javascript, Is now way more efficient and easier to read.
    -Added a "Misc" Button and Panel section
    -Started Commenting more of the code to make it easier to edit,
    -Worked on making it easier to expand on,.
    -Added Computer Science Section
    -Improved Themeing

--- script.js
+++ script.js
@@ -1,77 +1,24 @@
 // Data Transmission
 // The Code is highly inefficient. It is not DRY because I didnt have time.
+//
 
-$('#b-con').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#con').fadeToggle(200);
+// On Button Press Toggles panel
+// Also Changes Button State (Changes Colour)
+$('button').on('click', function() {
+  var id = $(this).attr('data-buttonId');
+  $('#b-'+ id).toggleClass('active');
+  $('#' + id).fadeToggle(200);
 });
 
-$('#b-hardware').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#hardware').fadeToggle(200);
-});
+// On Click Of Panel Close panel
+// Also Changes Button State (Changes Colour)
 
-$('#b-topology').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#topology').fadeToggle(200);
-});
-
-$('#b-protocols').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#protocols').fadeToggle(200);
-});
-
-$('#b-extent').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#extent').fadeToggle(200);
-});
-
-$('#b-networks').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#networks').fadeToggle(200);
-});
-
-// The Internet
-$('#b-uses').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#uses').fadeToggle(200);
-});
-
-$('#b-history').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#history').fadeToggle(200);
-});
-
-$('#b-web').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#web').fadeToggle(200);
-});
-
-$('#b-websites').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#websites').fadeToggle(200);
-});
-
-$('#b-lmc').on('click', function() {
-  var id = $(this).attr('id');
-  $('#'+ id).toggleClass('active');
-  $('#lmc').fadeToggle(200);
-});
-
-// Click To Close
-
-$('.cpanels').on('click', function() {
-  var panelId = $(this).attr('id');
-  $('#b-'+panelId).toggleClass('active');
+$('.panel-heading').on('click', function() {
+  var panelId = $(this).parents('.cpanels').attr('id');
+  $('#b-'+  panelId).toggleClass('active');
   $('#'+panelId).fadeToggle(200);
 });
+
+
+// $('#b-'+  panelId).toggleClass('active');
+// $('#'+panelId).fadeToggle(200);
